b83cf54b75d8da888a9f2ed0b295f8fe6de1ca8e
hsuhninhtet <60997423+hsuhninhtet@users.noreply.github.com>
2020-02-15 12:59:44 +0630

    Update index.js

--- index.js
+++ index.js
@@ -7,29 +7,7 @@ const BotEvents = require('viber-bot').Events
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(Example app listening on port ${port}!))
-
-//start_here
-const bot = new ViberBot({
-	authToken: '4b066d234267dd8f-8c4488c87a546514-2f2d5d9dc4e94444',
-	name: "EchoBot",
-	avatar: "http://viber.com/avatar.jpg" // It is recommended to be 720x720, and no more than 100kb.
-});
-
-// Perfect! Now here's the key part:
-bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
-	// Echo's back the message to the client. Your bot logic should sit here.
-	response.send(message);
-});const express = require('express')
-const app = express()
-const port = 3000
-
-const ViberBot = require('viber-bot').Bot;
-const BotEvents = require('viber-bot').Events
-
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(port, () => console.log(Example app listening on port ${port}!))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 //start_here
 const bot = new ViberBot({
