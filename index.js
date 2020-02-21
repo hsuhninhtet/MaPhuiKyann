88a62c78cb2e1ebcfbaf62302b4dcf07054878cb
hsuhninhtet <60997423+hsuhninhtet@users.noreply.github.com>
2020-02-21 10:12:36 +0630

    test

--- index.js
+++ index.js
@@ -1,23 +1,62 @@
-const express = require('express')
-const app = express()
-const PORT = 3000 
+const ViberBot  = require('viber-bot').Bot;
+const BotEvents = require('viber-bot').Events;
+const TextMessage = require('viber-bot').Message.Text;
+const RichMediaMessage = require('viber-bot').Message.RichMedia;
+const winston = require('winston');
+const wcf = require('winston-console-formatter');
+var request = require('request');
 
-const ViberBot = require('viber-bot').Bot;
-const BotEvents = require('viber-bot').Events
 
-app.get('/', (req, res) => { res.send('Hello World!') })
-
-app.listen(PORT || process.env.PORT, () => { console.log(`Example app listening on port ${PORT}!`) })
-
-//start_here
-const bot = new ViberBot({
-	authToken: '4b066d234267dd8f-8c4488c87a546514-2f2d5d9dc4e94444',
-	name: "EchoBot",
-	avatar: "http://viber.com/avatar.jpg" // It is recommended to be 720x720, and no more than 100kb.
+const logger = winston.createLogger({
+  level: 'debug',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [    
+    //new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.Console()
+  ]
 });
 
-// Perfect! Now here's the key part:
-bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
-	// Echo's back the message to the client. Your bot logic should sit here.
-	response.send(message);
+// Creating the bot with access token, name and avatar
+const bot = new ViberBot(logger, {
+    authToken: process.env.AUTH_TOKEN, // <--- Paste your token here
+    name: "To Do App",  // <--- Your bot name here
+    avatar: "http://api.adorable.io/avatar/200/isitup" // It is recommended to be 720x720, and no more than 100kb.
 });
+
+if (process.env.NOW_URL || process.env.HEROKU_URL) {
+    const http = require('http');
+    const port = process.env.PORT || 8080;
+
+    http.createServer(bot.middleware()).listen(port, () => bot.setWebhook(process.env.NOW_URL || process.env.HEROKU_URL));
+} else {
+    logger.debug('Could not find the now.sh/Heroku environment variables. Please make sure you followed readme guide.');
+}
+
+
+bot.onSubscribe(response => {
+    say(response, `Hi there ${response.userProfile.name}. I am ${bot.name}! Feel free to ask me if a web site is down for everyone or just you. Just send me a name of a website and I'll do the rest!`);
+});
+
+
+bot.onTextMessage(/^hi|hello$/i, (message, response) =>
+    response.send(new TextMessage(`Hi there ${response.userProfile.name}. I am robot`)));
+
+
+
+bot.onTextMessage(/./, (message, response) => {
+    const text = message.text.toLowerCase();
+    
+    switch(text){        
+        case "add":
+            //do something
+            break;
+        default:
+           //default
+                
+            
+    }
+});
+
+
+
