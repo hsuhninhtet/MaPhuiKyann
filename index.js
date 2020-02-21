2a2c88d002d0f70a3be07f4a009985d270c0e9c4
hsuhninhtet <60997423+hsuhninhtet@users.noreply.github.com>
2020-02-21 13:44:01 +0630

    test

--- index.js
+++ index.js
@@ -53,14 +53,14 @@ bot.onTextMessage(/mingalarpar/i, (message, response) =>{
 			   "Columns":6,
 			   "Rows":3,
 			   "ActionType":"open-url",
-			   "ActionBody":"မင်္ဂလာပါရှင်။ 
-			   မဖီးကြမ်းမှ ကြိုဆိုပါတယ်။ လူကြီးမင်းတို့ အလိုရှိရာ ငှက်ပျောသီးများကို ဝယ်ယူရရှိနိုင်ပါတယ်၊ ပစ္စည်းအရည်အသွေးကောင်းမွန်မှုနှင့် လူကြီးမင်းတို့စိတ်တိုင်းကျဝယ်ယူရရှိနိုင်ရေးသည် မဖီးကြမ်းလေးအတွက် ပထမဖြစ်ပါသည်။ organic ပစ္စည်းများဖြစ်ပါတယ်ရှင့်။",
+			   "ActionBody":"https://www.google.com",
 			   "Image":"https://techcrunch.com/wp-content/uploads/2020/01/Screen-Shot-2020-01-06-at-5.28.28-PM.png?w=730&crop=1"
 			},
 			{
 			   "Columns":6,
 			   "Rows":2,
-			   "Text":"<font color=#323232><b>Headphones with Microphone, On-ear Wired earphones</b></font><font color=#777777><br>Sound Intone </font><font color=#6fc133>$17.99</font>",
+			   "Text":"မင်္ဂလာပါရှင်။ 
+			   မဖီးကြမ်းမှ ကြိုဆိုပါတယ်။ လူကြီးမင်းတို့ အလိုရှိရာ ငှက်ပျောသီးများကို ဝယ်ယူရရှိနိုင်ပါတယ်၊ ပစ္စည်းအရည်အသွေးကောင်းမွန်မှုနှင့် လူကြီးမင်းတို့စိတ်တိုင်းကျဝယ်ယူရရှိနိုင်ရေးသည် မဖီးကြမ်းလေးအတွက် ပထမဖြစ်ပါသည်။ organic ပစ္စည်းများဖြစ်ပါတယ်ရှင့်။",
 			   "ActionType":"open-url",
 			   "ActionBody":"https://www.google.com",
 			   "TextSize":"medium",
